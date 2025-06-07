273bd9f044d8bbd64d95058aa9948f2e4e895291
busyforest <22302010017@m.fudan.edu.cn>
2025-06-07 13:18:54 +0800

    添加了结果输出门

--- src/app/gate-list/gate-list.component.ts
+++ src/app/gate-list/gate-list.component.ts
@@ -15,9 +15,11 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
 })
 export class GateListComponent {
   gates: Gate[] = [
-    {id: 1, name: 'AND', input: 0, output: 0},
-    {id: 2, name: 'OR', input: 0, output: 0},
-    {id: 3, name: 'NOT', input: 0, output: 0},
+    {id: 1, name: 'AND', input: 0, output: 0, icon:'/assets/gates/and.png'},
+    {id: 2, name: 'OR', input: 0, output: 0, icon:'/assets/gates/or.png'},
+    {id: 3, name: 'NOT', input: 0, output: 0, icon:'/assets/gates/not.png'},
+    {id: 4, name: 'INPUT', input: 0, output: 0, icon:'/assets/gates/input.png'},
+    {id: 5, name: 'OUTPUT', input: 0, output: 0, icon:'/assets/gates/output.png'},
   ]
   onDragStart(event: DragEvent, gate: Gate) {
     event.dataTransfer?.setData('application/json', JSON.stringify(gate));
